5b44dd0a5235624375a0a25cffa2b9c66315b947
collins kipkemoi <collinskipkemoi24@gmail.com>
2019-10-24 13:56:32 +0300

    Completed part 2

---
+++ src/app/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Http,Response} from "@angular/http"
+import {map} from 'rxjs/operators';
+
+
+@Injectable()
+export class ProductService {
+  private _albumUrl:string='../assets/album.json';
+
+  constructor(private _http:Http) { }
+
+  getAlbum(id:number){
+    return this._http.get(this._albumUrl).pipe(
+      map( response=>response.json())
+    )
+  }
+
+}
